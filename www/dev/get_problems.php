702897f6c384b8e230e55cfdf79bda477d28b4ac
kk <kevin830222@gmail.com>
2015-06-16 20:41:53 +0800

    修改設定

--- www/dev/get_problems.php
+++ www/dev/get_problems.php
@@ -1,5 +1,5 @@
 <?php
-    $conn=mysqli_connect("localhost","root","0222","cnldb");
+    $conn=mysqli_connect("localhost","root","cnlroot","cnldb");
     if(mysqli_connect_errno()){
         echo "Failed to connect to MySQL: ",mysqli_connect_error();
         exit(0);
